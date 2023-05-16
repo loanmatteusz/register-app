26c6714d50000a0fe3dbfb740c18471a05173e54
Loan Matteus <loan.matteusz@gmail.com>
2023-05-16 14:53:56 -0300

    fix: xs size responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,9 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
     return (
-        <div className={` flex flex-col w-2/3 bg-white text-gray-800 rounded-md`}>
+        <div className={`flex flex-col max-sm:min-w-[95%] w-2/3 bg-white text-gray-800 rounded-md`}>
             <Title>{props.title}</Title>
-            <div className={`p-6`}>
+            <div className={`w-full max-sm:p-2 p-6`}>
                 {props.children}
             </div>
         </div>
